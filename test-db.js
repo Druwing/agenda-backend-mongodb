800b6b3741e56188e70e0d693b79d9878c742531
Druwing <143631126+Druwing@users.noreply.github.com>
2025-04-29 17:26:59 -0300

    Delete test-db.js

--- test-db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/jwt-auth')
-  .then(() => console.log('Conectado!'))
-  .catch(err => console.error('Erro:', err));
