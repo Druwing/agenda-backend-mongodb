2c4d996e26fd73f752a4151d15edb1360300f3dd
Druwing <143631126+Druwing@users.noreply.github.com>
2025-04-29 18:04:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const logger = require('./src/utils/logger');
 
 const app = express();
 
+const mongooseUrl = process.env.MONGODB_URI_ATLAS || config.mongoose.url;
+await mongoose.connect(mongooseUrl, config.mongoose.options);
+
 // Middlewares
 app.use(cors());
 app.use(helmet());
