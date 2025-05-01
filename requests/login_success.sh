1525d16b24faac35cfea70460e4c9a61c89c9b60
Druwing <143631126+Druwing@users.noreply.github.com>
2025-05-01 09:56:19 -0300

    Delete requests directory

--- requests/login_success.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:3000/api/auth/login \
--H "Content-Type: application/json" \
--d '{
-  "email": "john.doe@example.com",
-  "password": "securePassword123"
-}'
--- requests/register_success.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://localhost:3000/api/auth/register \
--H "Content-Type: application/json" \
--d '{
-  "name": "John Doe",
-  "email": "john.doe@example.com",
-  "password": "securePassword123"
-}'
